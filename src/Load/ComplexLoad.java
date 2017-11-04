a237b64db31dffd011b3101258daedf44f8cbc46
yaoquping <yaoquping@jd..com>
2017-11-04 15:03:49 +0800

    java类加载机制

---
+++ src/Load/ComplexLoad.java
@@ -0,0 +1,29 @@
+package Load;
+
+public class ComplexLoad {
+    public static int k = 0;
+    public static ComplexLoad ob1 = new ComplexLoad("ob1");
+    public static ComplexLoad ob2 = new ComplexLoad("ob2");
+    public static int i = print("i");
+    public static int n = 99;
+    public int j = print("j");
+    {
+        print("construct block");
+    }
+    static {
+        print("static block");
+    }
+    public ComplexLoad(String str) {
+        System.out.println((++k) + ":" + str + "   i=" + i + "    n=" + n);
+        ++i;
+        ++n;
+    }
+    public static int print(String str) {
+        System.out.println((++k) + ":" + str + "   i=" + i + "    n=" + n);
+        ++n;
+        return ++i;
+    }
+    public static void main(String args[]) {
+    	ComplexLoad complexLoad = new ComplexLoad("init");
+    }
+}
---
+++ src/Load/Exm1.java
@@ -0,0 +1,17 @@
+package Load;
+import java.lang.*;
+/**
+ * Exm1类在加载时,jvm为保证Float是Number的子类不得不先加载这两个类，但不初始化
+ */
+public class Exm1 {
+	
+	public Number number;
+	
+	public Exm1(Float f){
+		number = f;
+	}
+	public static void main(String[] args) {
+		
+	}
+    
+}
