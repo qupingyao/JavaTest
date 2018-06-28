0cfd9abc38d061467d906d4ed5bcb7c04aab6fe9
yaoquping <yaoquping@jd..com>
2018-06-28 16:36:46 +0800

    版本迁移

---
+++ src/spring/A.java
@@ -0,0 +1,10 @@
+package spring;
+
+public class A implements AInterface{
+
+	@Override
+	public void say() {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/spring/AInterface.java
@@ -0,0 +1,7 @@
+package spring;
+
+public interface AInterface {
+	
+	public void say();
+	
+}
